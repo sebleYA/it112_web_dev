27815f3238aefc3d284149e9635b883e71b03e94
Seblew <seblew@Seblews-MacBook-Air.local>
2025-04-15 22:55:46 -0700

    modified app.py by adding about page

--- my_flask_blog/app.py
+++ my_flask_blog/app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, This is my flask app"
+    return "<h1>Hello, This is my flask app<h1>"
+
+@app.route("/about")
+def about():
+    return "<p>Hi, welcome to my page, My name is Seblewongel but you can call me Seble<p>"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
