12f2a2328c7b23881e30aca738e50c9a0143f04a
Seblew <seblew@Seblews-MacBook-Air.local>
2025-06-15 21:25:44 -0700

    updated to register my book model into admin

--- django_app/myapp/admin.py
+++ django_app/myapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'year_published')  # shown in the list view
+    search_fields = ('title', 'author')                   # enables search box
+    list_filter = ('year_published',)                     # adds sidebar filter
