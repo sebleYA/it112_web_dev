f66ba392e7446317893760bfa32281ba8b7a2e73
Seblew <seblew@Seblews-MacBook-Air.local>
2025-06-15 21:00:21 -0700

    updated adding the api routes for books, single book and for creating new book

--- django_app/myapp/urls.py
+++ django_app/myapp/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('books/', views.book_list, name='book_list'),
     path('books/<int:book_id>/', views.book_detail, name='book_detail'),
+     # API Routes
+    path('api/books/', views.api_all_books),
+    path('api/book/', views.api_single_book),
+    path('api/book/', views.api_create_book),  # Same path as above, but for POST
 ]
